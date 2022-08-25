3e637dd8bdb884b2d63bd962b79a33fccadbbb61
Aibek_kz <pigeonthe@gmail.com>
2022-08-25 21:49:03 +0600

    router with nothing

---
+++ cmd/client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./web/src"))
+	http.Handle("/src/", http.StripPrefix("/src/", fs))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		indexTmpl, err := template.ParseFiles("./web/index.html")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+		if err := indexTmpl.Execute(w, nil); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+	})
+	log.Printf("Frontend server is starting at %v", "8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ web/src/index.js
@@ -0,0 +1,8 @@
+const router = async () => {
+    const routes = [
+        {path: "/", view: () => console.log("Viewing Dashboard")},
+        {path: "/posts", view: () => console.log("Viewing Posts")},
+        {path: "settings", view: () =>console.log("Viewing Settings")}
+    ];
+    const potenti
+};
