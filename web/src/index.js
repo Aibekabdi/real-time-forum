53374d2fe7f57f87a826bdf0f7656328893839c9
Aibek_kz <pigeonthe@gmail.com>
2022-08-27 20:14:45 +0600

    view files add

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,54 @@
+import Post from "./views/PostView.js";
+import Chats from "./views/ChatsView.js";
+import Signin from "./views/SigninView.js";
+import Signup from "./views/SignupView.js";
+
+
+
+const navigateTo = url => {
+    history.pushState(null , null, url);
+    router();
+};
+
 const router = async () => {
     const routes = [
-        {path: "/", view: () => console.log("Viewing Dashboard")},
-        {path: "/posts", view: () => console.log("Viewing Posts")},
-        {path: "settings", view: () =>console.log("Viewing Settings")}
+        {path: "/", view: Post},
+        {path: "/sign-in", view: Signin},
+        {path: "/sign-up", view: Signup},
+        {path: "/chats", view: Chats},
     ];
-    const potenti
+
+    //
+    const potentialMatches = routes.map(route => {
+        return {
+            route : route,
+            isMatch: location.pathname === route.path
+        };
+    });
+    
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+    
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+    
+    const view = new match.route.view();
+    document.querySelector("#app").innerHTML = await view.getHtml();
+
 };
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")){
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    })
+    
+    router();
+})
