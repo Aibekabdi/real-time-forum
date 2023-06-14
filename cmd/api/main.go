588555a9cdaeb7c40e58b3144eb773e2722d4277
Aibek_kz <pigeonthe@gmail.com>
2023-06-15 04:31:34 +0600

    config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"forum/pkg/utils"
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Preparing environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("err loading: %v", err)
+	}
+	// Load configuration from config/config.json which contains details such as DB connection params
+	conf, err := utils.NewConfig("./configs/config.json")
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	log.Println(conf)
 }
--- pkg/utils/config.go
+++ pkg/utils/config.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type (
 	Conf struct {
 		Api      Api      `json:"api"`
@@ -15,5 +20,24 @@ type (
 		Port string `json:"port"`
 	}
 	Database struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+		Host     string `json:"host"`
+		Port     string `json:"port"`
+		DBName   string `json:"dbname"`
+		SSLMode  string `json:"sslmode"`
 	}
 )
+
+// loading config
+func NewConfig(path string) (*Conf, error) {
+	var newConfig Conf
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.NewDecoder(file).Decode(&newConfig); err != nil {
+		return nil, err
+	}
+	return &newConfig, nil
+}
