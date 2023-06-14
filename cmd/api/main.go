814b348dd2854ee1a970bb51a48e090c6523c36d
Aibek_kz <pigeonthe@gmail.com>
2023-06-15 05:00:37 +0600

    postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"forum/pkg/postgres"
 	"forum/pkg/utils"
 	"log"
 
@@ -16,8 +17,20 @@ func main() {
 	// Load configuration from config/config.json which contains details such as DB connection params
 	conf, err := utils.NewConfig("./configs/config.json")
 	if err != nil {
+		log.Fatalf("failed to load configs: %s", err.Error())
+	}
+
+	// Connect to the postgres DB
+	db, err := postgres.NewPostgresDB(conf.Database)
+	if err != nil {
 		log.Fatalf("failed to initialize db: %s", err.Error())
 	}
 
-	log.Println(conf)
+	defer func() {
+		if err = db.Close(); err != nil {
+			log.Fatal("can't close connection db, err:", err)
+		} else {
+			log.Println("db closed")
+		}
+	}()
 }
---
+++ pkg/postgres/postgres.go
@@ -0,0 +1,30 @@
+package postgres
+
+import (
+	"fmt"
+	"forum/pkg/utils"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresDB(cfg utils.Database) (*sqlx.DB, error) {
+	// Connect to the PostgreSQL database
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		cfg.Host,
+		cfg.Port,
+		cfg.Username,
+		cfg.DBName,
+		cfg.Password,
+		cfg.SSLMode,
+	))
+	if err != nil {
+		return nil, err
+	}
+
+	// Ping the database to ensure a connection
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
