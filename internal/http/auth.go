24c3985425ff090651a03df8146313af9690e6c9
Aibek_kz <pigeonthe@gmail.com>
2023-06-19 03:14:32 +0600

    changed name of files

---
+++ internal/http/auth.go
@@ -0,0 +1,44 @@
+package http
+
+import (
+	"forum/internal/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) signUp(c *gin.Context) {
+	var input models.User
+
+	if err := c.BindJSON(&input); err != nil {
+		h.errorResponse(c, http.StatusBadRequest, "invalid input body")
+		return
+	}
+
+	// Validating user input and adding to db
+	if err := h.service.Auth.Create(c.Request.Context(), input); err != nil {
+		h.errorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, statusResponse{Status: "OK"})
+}
+
+func (h *Handler) signIn(c *gin.Context) {
+	var input models.User
+
+	if err := c.BindJSON(&input); err != nil {
+		h.errorResponse(c, http.StatusBadRequest, "invalid input body")
+		return
+	}
+
+	token, err := h.service.Auth.SignIn(c.Request.Context(), input)
+	if err != nil {
+		h.errorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
+}
